44634b87cd96293b350c8066154e5a93204fea3d
User <User@DESKTOP-0M1BP49>
2023-01-09 18:56:45 +0100

    Add project files.

---
+++ JWTAuthentication/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using JWTAuthentication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthentication.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class LoginController : ControllerBase
+    {
+
+        private readonly JwtAuthenticationManager jwtAuthenticationManager;
+
+        public LoginController(JwtAuthenticationManager jwtAuthenticationManager)
+        {
+            this.jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Authorize([FromBody] User user)
+        {
+            var token = jwtAuthenticationManager.Authenticate(user.UserName, user.Password);
+
+            if (string.IsNullOrEmpty(token))
+                return Unauthorized();
+            return Ok(token);
+        }
+
+        [HttpGet]
+        public IActionResult TestRoute()
+        {
+            return Ok("Authorized");
+        }
+    }
+}
